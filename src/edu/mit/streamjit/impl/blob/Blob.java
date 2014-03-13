4167fca2b8b32312dbacb0e5545e391faaae9dd4
sumanan <sumanan@csail.mit.edu>
2014-03-13 19:58:29 +0800

    Blob.getMinimumInitBufferCapacity() added

--- src/edu/mit/streamjit/impl/blob/Blob.java
+++ src/edu/mit/streamjit/impl/blob/Blob.java
@@ -55,6 +55,8 @@ public interface Blob {
 
 	public int getMinimumSteadyBufferCapacity(Token token);
 
+	public int getMinimumInitBufferCapacity(Token token);
+
 	/**
 	 * Installs buffers for this Blob's input and output edges.
 	 * @param buffers an immutable map of tokens to the buffer on the corresponding
