3612544cbd2798699eb9484165d5a341480c08eb
sumanan <sumanan@csail.mit.edu>
2015-02-06 11:51:07 +0800

    BufferSizeCalc added and sendNewbufSizes2() moved.
    BufferSizeCalc class takes the responsibility to calculate the deadlock free
    buffer sizes. StreamJitAppManager uses BufferSizeCalc.

--- src/edu/mit/streamjit/impl/distributed/StreamJitAppManager.java
+++ src/edu/mit/streamjit/impl/distributed/StreamJitAppManager.java
@@ -296,7 +296,11 @@ public class StreamJitAppManager {
 		setupHeadTail(conInfoMap, app.bufferMap, multiplier);
 
 		ciP.waitforBufSizes();
-		sendNewbufSizes2();
+		ImmutableMap<Token, Integer> finalInputBuf = BufferSizeCalc
+				.sendNewbufSizes2(ciP.bufSizes, app);
+		CTRLRMessageElement me = new CTRLCompilationInfo.FinalBufferSizes(
+				finalInputBuf);
+		controller.sendToAll(me);
 
 		boolean isCompiled = apStsPro.waitForCompilation();
 		app.eLogger.eEvent("compilation");
@@ -477,130 +481,6 @@ public class StreamJitAppManager {
 		for (Map.Entry<Token, Integer> en : finalInputBuf.entrySet()) {
 			System.out.println(en.getKey() + " - " + en.getValue());
 		}
-
-		CTRLRMessageElement me = new CTRLCompilationInfo.FinalBufferSizes(
-				finalInputBuf);
-		controller.sendToAll(me);
-	}
-
-		/**
-	 * Calculates the input buffer sizes to avoid deadlocks. Added on
-	 * [2014-03-07]. Finds out the buffer sizes through ILP solving.
-	 * {@link #sendNewbufSizes()} doesn't guarantee deadlock freeness.
-	 */
-	private void sendNewbufSizes2() {
-		class bufInfo {
-			int steadyInput;
-			int steadyOutput;
-			int initInput;
-			int initOutput;
-			Variable outVar;
-			Variable inVar;
-
-			void addconstrain(ILPSolver solver) {
-				LinearExpr exp = outVar.asLinearExpr(steadyOutput).minus(
-						steadyInput, inVar);
-				solver.constrainAtLeast(exp,
-						(initInput + steadyInput - initOutput));
-			}
-		}
-
-		Map<Token, Integer> minInitInputBufCapacity = new HashMap<>();
-		Map<Token, Integer> minInitOutputBufCapacity = new HashMap<>();
-		Map<Token, Integer> minSteadyInputBufCapacity = new HashMap<>();
-		Map<Token, Integer> minSteadyOutputBufCapacity = new HashMap<>();
-		ImmutableMap.Builder<Token, Integer> finalInputBufCapacity = new ImmutableMap.Builder<>();
-
-		for (BufferSizes b : ciP.bufSizes.values()) {
-			minInitInputBufCapacity.putAll(b.minInitInputBufCapacity);
-			minInitOutputBufCapacity.putAll(b.minInitOutputBufCapacity);
-			minSteadyInputBufCapacity.putAll(b.minSteadyInputBufCapacity);
-			minSteadyOutputBufCapacity.putAll(b.minSteadyOutputBufCapacity);
-		}
-
-		ILPSolver solver = new ILPSolver();
-		Map<Token, bufInfo> bufInfos = new HashMap<>();
-		Map<Token, Variable> variables = new HashMap<>();
-		for (Token blob : app.blobGraph.getBlobIds()) {
-			Variable v = solver.newVariable(blob.toString());
-			variables.put(blob, v);
-			LinearExpr expr = v.asLinearExpr(1);
-			solver.constrainAtLeast(expr, 1);
-			Set<Token> outputs = app.blobGraph.getOutputs(blob);
-			for (Token out : outputs) {
-				if (out.isOverallOutput())
-					continue;
-				bufInfo b = new bufInfo();
-				b.initOutput = minInitOutputBufCapacity.get(out);
-				b.steadyOutput = minSteadyOutputBufCapacity.get(out);
-				b.outVar = v;
-				bufInfos.put(out, b);
-			}
-		}
-
-		for (Token blob : app.blobGraph.getBlobIds()) {
-			Set<Token> inputs = app.blobGraph.getInputs(blob);
-			Variable v = variables.get(blob);
-			if (v == null)
-				throw new IllegalStateException("No variable");
-			for (Token in : inputs) {
-				if (in.isOverallInput())
-					continue;
-				bufInfo b = bufInfos.get(in);
-				if (b == null)
-					throw new IllegalStateException("No buffer info");
-				b.initInput = minInitInputBufCapacity.get(in);
-				b.steadyInput = minSteadyInputBufCapacity.get(in);
-				b.inVar = v;
-				b.addconstrain(solver);
-			}
-		}
-
-		LinearExpr lf = null;
-		for (Variable v : variables.values()) {
-			if (lf == null)
-				lf = v.asLinearExpr(1);
-			else
-				lf = lf.plus(1, v);
-		}
-		solver.minimize(lf);
-		solver.solve();
-
-		for (Token blob : app.blobGraph.getBlobIds()) {
-			int mul = variables.get(blob).value();
-			Set<Token> outputs = app.blobGraph.getOutputs(blob);
-			// System.out.println("Multiplication factor of blob "
-			// + blob.toString() + " is " + mul);
-			for (Token out : outputs) {
-				if (out.isOverallOutput())
-					continue;
-				int initOut = minInitOutputBufCapacity.get(out);
-				int steadyOut = minSteadyOutputBufCapacity.get(out);
-				int initIn = minInitInputBufCapacity.get(out);
-				int steadyIn = minSteadyInputBufCapacity.get(out);
-				int inMax = Math.max(initIn, steadyIn);
-				int newInSize = Math.max(initOut + steadyOut * mul, inMax);
-				finalInputBufCapacity.put(out, newInSize);
-			}
-		}
-
-		ImmutableMap<Token, Integer> finalInputBuf = finalInputBufCapacity
-				.build();
-
-		/*
-		 * System.out
-		 * .println("InputBufCapacity \t\t - Init \t\t - Steady \t\t - final");
-		 * for (Map.Entry<Token, Integer> en :
-		 * minInitInputBufCapacity.entrySet()) { System.out.println(en.getKey()
-		 * + "\t\t - " + en.getValue() + "\t\t - " +
-		 * minSteadyInputBufCapacity.get(en.getKey()) + "\t\t - " +
-		 * finalInputBuf.get(en.getKey())); }
-		 * System.out.println("minOutputBufCapacity requirement"); for
-		 * (Map.Entry<Token, Integer> en : minInitOutputBufCapacity.entrySet())
-		 * { System.out.println(en.getKey() + "\t\t - " + en.getValue() +
-		 * "\t\t - " + minSteadyOutputBufCapacity.get(en.getKey()) + "\t\t - " +
-		 * finalInputBuf.get(en.getKey())); }
-		 */
 
 		CTRLRMessageElement me = new CTRLCompilationInfo.FinalBufferSizes(
 				finalInputBuf);
