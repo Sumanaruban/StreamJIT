c01fddf2838807783589b6eddad2ba74d1bfc31d
sumanan <sumanan@csail.mit.edu>
2015-02-06 11:55:52 +0800

    Renamed:sendNewbufSizes2()->finalInputBufSizes()

--- src/edu/mit/streamjit/impl/distributed/StreamJitAppManager.java
+++ src/edu/mit/streamjit/impl/distributed/StreamJitAppManager.java
@@ -296,7 +296,7 @@ public class StreamJitAppManager {
 
 		ciP.waitforBufSizes();
 		ImmutableMap<Token, Integer> finalInputBuf = BufferSizeCalc
-				.sendNewbufSizes2(ciP.bufSizes, app);
+				.finalInputBufSizes(ciP.bufSizes, app);
 		CTRLRMessageElement me = new CTRLCompilationInfo.FinalBufferSizes(
 				finalInputBuf);
 		controller.sendToAll(me);
