2115cfb6a0c073ca2654041100e2d9de30f6e33b
sumanan <sumanan@csail.mit.edu>
2015-02-06 12:01:21 +0800

    sendNewbufSizes() moved to BufferSizeCalc.
    sendNewbufSizes() moved from StreamJitAppManager to BufferSizeCalc.

--- src/edu/mit/streamjit/impl/distributed/BufferSizeCalc.java
+++ src/edu/mit/streamjit/impl/distributed/BufferSizeCalc.java
@@ -8,7 +8,10 @@ import com.google.common.collect.ImmutableMap;
 
 import edu.mit.streamjit.impl.blob.Blob.Token;
 import edu.mit.streamjit.impl.blob.Buffer;
+import edu.mit.streamjit.impl.distributed.common.CTRLCompilationInfo;
+import edu.mit.streamjit.impl.distributed.common.CTRLRMessageElement;
 import edu.mit.streamjit.impl.distributed.common.CompilationInfo.BufferSizes;
+import edu.mit.streamjit.impl.distributed.runtimer.Controller;
 import edu.mit.streamjit.util.ilpsolve.ILPSolver;
 import edu.mit.streamjit.util.ilpsolve.ILPSolver.LinearExpr;
 import edu.mit.streamjit.util.ilpsolve.ILPSolver.Variable;
@@ -148,5 +151,73 @@ public class BufferSizeCalc {
 		 * finalInputBuf); controller.sendToAll(me);
 		 */
 		return finalInputBuf;
+	}
+
+	/**
+	 * [6 Feb, 2015] Moved from {@link StreamJitAppManager}. This method is not
+	 * in use. Lets keep this for a while.
+	 * <p>
+	 * Calculates the input buffer sizes to avoid deadlocks. Added on
+	 * [2014-03-01]
+	 */
+	private void sendNewbufSizes(Map<Integer, BufferSizes> bufSizes,
+			StreamJitApp<?, ?> app, Controller controller) {
+		Map<Token, Integer> minInputBufCapacity = new HashMap<>();
+		Map<Token, Integer> minOutputBufCapacity = new HashMap<>();
+		ImmutableMap.Builder<Token, Integer> finalInputBufCapacity = new ImmutableMap.Builder<>();
+		Map<Token, Integer> IORatio = new HashMap<>();
+
+		for (BufferSizes b : bufSizes.values()) {
+			minInputBufCapacity.putAll(b.minInitInputBufCapacity);
+			minOutputBufCapacity.putAll(b.minInitOutputBufCapacity);
+		}
+		System.out.println("minInputBufCapacity requirement");
+		for (Map.Entry<Token, Integer> en : minInputBufCapacity.entrySet()) {
+			System.out.println(en.getKey() + " - " + en.getValue());
+		}
+		System.out.println("minOutputBufCapacity requirement");
+		for (Map.Entry<Token, Integer> en : minOutputBufCapacity.entrySet()) {
+			System.out.println(en.getKey() + " - " + en.getValue());
+		}
+
+		for (Token t : minInputBufCapacity.keySet()) {
+			if (t.isOverallInput())
+				continue;
+			int outSize = minOutputBufCapacity.get(t);
+			int inSize = minInputBufCapacity.get(t);
+			IORatio.put(t, (int) Math.ceil(((double) inSize) / outSize));
+		}
+
+		for (Token blob : app.blobGraph.getBlobIds()) {
+			int mul = 1;
+			Set<Token> outputs = app.blobGraph.getOutputs(blob);
+			for (Token out : outputs) {
+				if (out.isOverallOutput())
+					continue;
+				mul = Math.max(mul, IORatio.get(out));
+			}
+			System.out.println("Multiplication factor of blob "
+					+ blob.toString() + " is " + mul);
+			for (Token out : outputs) {
+				if (out.isOverallOutput())
+					continue;
+				int outSize = minOutputBufCapacity.get(out);
+				int inSize = minInputBufCapacity.get(out);
+				int newInSize = Math.max(outSize * mul, inSize);
+				finalInputBufCapacity.put(out, newInSize);
+			}
+		}
+
+		ImmutableMap<Token, Integer> finalInputBuf = finalInputBufCapacity
+				.build();
+
+		System.out.println("finalInputBufCapacity");
+		for (Map.Entry<Token, Integer> en : finalInputBuf.entrySet()) {
+			System.out.println(en.getKey() + " - " + en.getValue());
+		}
+
+		CTRLRMessageElement me = new CTRLCompilationInfo.FinalBufferSizes(
+				finalInputBuf);
+		controller.sendToAll(me);
 	}
 }
