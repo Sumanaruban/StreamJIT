f1d9467d5dfd1e2f3cd5974075d7011e21733c92
sumanan <sumanan@csail.mit.edu>
2015-03-16 12:51:16 +0800

    Analyze cfgs and prints the summary
    Prints the summary in impactful parameter type order.

--- src/edu/mit/streamjit/impl/common/ConfigurationAnalyzer.java
+++ src/edu/mit/streamjit/impl/common/ConfigurationAnalyzer.java
@@ -3,8 +3,12 @@ package edu.mit.streamjit.impl.common;
 import java.io.File;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import com.google.common.collect.ImmutableSet;
@@ -19,11 +23,11 @@ import edu.mit.streamjit.util.ConfigurationUtils;
 public class ConfigurationAnalyzer {
 
 	public static void main(String[] args) {
-		ConfigurationAnalyzer ca = new ConfigurationAnalyzer(
-				"NestedSplitJoinCore");
+		ConfigurationAnalyzer ca = new ConfigurationAnalyzer("FMRadioCore");
+		ca.Analyze(50);
 		// ca.compare(3, 4);
 
-		System.out.println(ca.getRunningTime("NestedSplitJoinCore", 3));
+		// System.out.println(ca.getRunningTime("NestedSplitJoinCore", 3));
 	}
 
 	private final String cfgDir;
@@ -179,13 +183,69 @@ public class ConfigurationAnalyzer {
 	}
 
 	private void Analyze(int maxTuneCount) {
+		// maxTuneCount = 10;
+		List<ComparisionSummary> comparitionSummaryList = new ArrayList<>();
 		for (int i = 1; i < maxTuneCount - 1; i++) {
 			double t1 = getRunningTime(appName, i);
 			double t2 = getRunningTime(appName, i + 1);
 			if (needTocompare(t1, t2)) {
-				System.out.println("Comparing...");
-				compare(i, i + 1);
+				// System.out.println("Comparing...");
+				ComparisionSummary sum = new ComparisionSummary(i, i + 1, t1,
+						t2);
+				int diffCount = compare2(i, i + 1, sum);
+				// System.out.println(diffCount);
+				comparitionSummaryList.add(sum);
 			}
+		}
+		print(comparitionSummaryList);
+	}
+
+	private void print(List<ComparisionSummary> comparitionSummaryList) {
+		ParamMapSummary paramMapSum = new ParamMapSummary();
+		System.out.println("Total parameters in the configuration = "
+				+ paramMapSum.totalCount);
+		for (ComparisionSummary s : comparitionSummaryList) {
+			List<ParamSummary> paramSummaryList = new ArrayList<>();
+			System.out.println(s);
+			int totalDiffs = s.toatalDiffCount;
+			double per1 = ((double) totalDiffs * 100) / paramMapSum.totalCount;
+			System.out.println(String.format(
+					"TotalParams=%d,TotalDiffs=%d,Per=%f",
+					paramMapSum.totalCount, totalDiffs, per1));
+			for (ParamType p : ParamType.values()) {
+				int totalCount = paramMapSum.parmTypeCount.get(p);
+				int diffCount = s.diffCount.get(p);
+				ParamSummary ps = new ParamSummary(p, totalCount, diffCount);
+				paramSummaryList.add(ps);
+				// System.out.println(ps);
+			}
+			Collections.sort(paramSummaryList);
+			for (ParamSummary ps : paramSummaryList)
+				System.out.println(ps);
+		}
+	}
+
+	private class ParamSummary implements Comparable<ParamSummary> {
+		final ParamType p;
+		final int totalCount;
+		final int diffCount;
+		final Double per;
+
+		ParamSummary(ParamType p, int totalCount, int diffCount) {
+			this.p = p;
+			this.totalCount = totalCount;
+			this.diffCount = diffCount;
+			this.per = ((double) diffCount * 100) / totalCount;
+		}
+
+		public String toString() {
+			return String.format("\t%s:tot=%d,diff=%d,per=%f", p, totalCount,
+					diffCount, per);
+		}
+
+		@Override
+		public int compareTo(ParamSummary o) {
+			return o.per.compareTo(per);
 		}
 	}
 
@@ -200,9 +260,33 @@ public class ConfigurationAnalyzer {
 			min = t2;
 			max = t1;
 		}
+		max = t1;
+		min = t2;
 		if (max / min > diff)
 			return true;
 		return false;
+	}
+
+	private int compare2(Integer first, Integer second, ComparisionSummary sum) {
+		Configuration cfg1 = readcoConfiguration(first);
+		Configuration cfg2 = readcoConfiguration(second);
+		int diffCount = 0;
+		Map<String, Parameter> paramMap = cfg1.getParametersMap();
+		// System.out.println("ParamMap size = " + paramMap.size());
+		for (Entry<String, Parameter> en : paramMap.entrySet()) {
+			Parameter p1 = en.getValue();
+			Parameter p2 = cfg2.getParameter(en.getKey());
+			if (p2 == null)
+				throw new IllegalStateException(String.format(
+						"No parameter %s in configuration2", en.getKey()));
+			if (!p1.equals(p2)) {
+				diffCount++;
+				sum.diff(p1);
+			}
+
+		}
+		sum.toatalDiffCount = diffCount;
+		return diffCount;
 	}
 
 	public enum ParamType {
@@ -243,18 +327,113 @@ public class ConfigurationAnalyzer {
 				return ImmutableSet.of("InternalArrayish");
 			}
 		},
-		EXTERNAL_STORAGE_STRATEGY {
-			@Override
-			public ImmutableSet<String> variablePrefixList() {
-				return ImmutableSet.of("ExternalArrayish", "UseDoubleBuffers");
-			}
-		},
+		// EXTERNAL_STORAGE_STRATEGY {
+		// @Override
+		// public ImmutableSet<String> variablePrefixList() {
+		// return ImmutableSet.of("ExternalArrayish", "UseDoubleBuffers");
+		// }
+		// },
 		MULTIPLIER {
 			@Override
 			public ImmutableSet<String> variablePrefixList() {
 				return ImmutableSet.of("multiplier");
 			}
+		},
+		UNROLL_CORE {
+			@Override
+			public ImmutableSet<String> variablePrefixList() {
+				return ImmutableSet.of("UnrollCore");
+			}
 		};
 		public abstract ImmutableSet<String> variablePrefixList();
+	}
+
+	public class ParamMapSummary {
+		int totalCount;
+		final Map<ParamType, Integer> parmTypeCount;
+		ParamMapSummary() {
+			parmTypeCount = new HashMap<>();
+			initilizeparmTypeCount();
+			count();
+		}
+
+		private void initilizeparmTypeCount() {
+			for (ParamType p : ParamType.values()) {
+				parmTypeCount.put(p, 0);
+			}
+		}
+
+		private void count() {
+			Configuration cfg1 = readcoConfiguration(1);
+			Map<String, Parameter> paramMap = cfg1.getParametersMap();
+			totalCount = paramMap.size();
+			for (Parameter p : paramMap.values())
+				diff(p);
+		}
+
+		void diff(Parameter param) {
+			for (ParamType p : ParamType.values()) {
+				for (String prefix : p.variablePrefixList()) {
+					if (param.getName().startsWith(prefix)) {
+						int count = parmTypeCount.get(p);
+						parmTypeCount.put(p, ++count);
+						return;
+					}
+				}
+			}
+			// System.err.println("No matches to " + param.toString());
+		}
+	}
+
+	private class ComparisionSummary {
+		final int firstCfg;
+		final int secondCfg;
+		final double t1;
+		final double t2;
+		int toatalDiffCount = 0;
+		// List<Parameter> diffParamList;
+		Map<ParamType, Integer> diffCount;
+		public ComparisionSummary(final int firstCfg, final int secondCfg,
+				final double t1, final double t2) {
+			this.firstCfg = firstCfg;
+			this.secondCfg = secondCfg;
+			this.t1 = t1;
+			this.t2 = t2;
+			initilizeDiffCount();
+		}
+
+		private void initilizeDiffCount() {
+			diffCount = new HashMap<>();
+			for (ParamType p : ParamType.values()) {
+				diffCount.put(p, 0);
+			}
+			// diffCount.put(ParamType.ALLOCATION_STRATEGY, 0);
+			// diffCount.put(ParamType.EXTERNAL_STORAGE_STRATEGY, 0);
+			// diffCount.put(ParamType.FUSION_STRATEGY, 0);
+			// diffCount.put(ParamType.INTERNAL_STORAGE_STRATEGY, 0);
+			// diffCount.put(ParamType.MULTIPLIER, 0);
+			// diffCount.put(ParamType.PARTITION, 0);
+			// diffCount.put(ParamType.REMOVAL_STRATEGY, 0);
+			// diffCount.put(ParamType.UNBOXING_STRATEGY, 0);
+		}
+
+		void diff(Parameter param) {
+			for (ParamType p : ParamType.values()) {
+				for (String prefix : p.variablePrefixList()) {
+					if (param.getName().startsWith(prefix)) {
+						int count = diffCount.get(p);
+						diffCount.put(p, ++count);
+						return;
+					}
+				}
+			}
+			// System.err.println("No matches to " + param.toString());
+		}
+
+		@Override
+		public String toString() {
+			return String.format("Cfg1=%d, Cfg2=%d, t1=%f, t2=%f", firstCfg,
+					secondCfg, t1, t2);
+		}
 	}
 }
