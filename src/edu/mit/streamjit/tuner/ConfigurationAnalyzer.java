fc6f10bcf2ef8f5164073a8eecadd2050ba574b6
sumanan <sumanan@csail.mit.edu>
2015-03-21 11:07:18 +0800

    Unused methods removed.

--- src/edu/mit/streamjit/tuner/ConfigurationAnalyzer.java
+++ src/edu/mit/streamjit/tuner/ConfigurationAnalyzer.java
@@ -16,10 +16,7 @@ import java.util.Map.Entry;
 import com.google.common.collect.ImmutableSet;
 
 import edu.mit.streamjit.impl.common.Configuration;
-import edu.mit.streamjit.impl.common.Configuration.FloatParameter;
-import edu.mit.streamjit.impl.common.Configuration.IntParameter;
 import edu.mit.streamjit.impl.common.Configuration.Parameter;
-import edu.mit.streamjit.impl.common.Configuration.SwitchParameter;
 import edu.mit.streamjit.impl.distributed.common.Utils;
 import edu.mit.streamjit.util.ConfigurationUtils;
 
@@ -57,84 +54,6 @@ public class ConfigurationAnalyzer {
 		this.appName = appName;
 		this.cfgDir = String.format("%s%s%s", appName, File.separator,
 				ConfigurationUtils.configDir);
-	}
-
-	private static void compare(FloatParameter p1, FloatParameter p2) {
-		float val1 = p1.getValue();
-		float val2 = p2.getValue();
-		if (val1 == val2)
-			System.out.println(String.format("%s: p1 = p2. value = %f",
-					p1.getName(), val1));
-		if (val1 > val2)
-			System.out.println(String.format("%s: p1 > p2. %f > %f",
-					p1.getName(), val1, val2));
-		else
-			System.out.println(String.format("%s: p1 < p2. %f < %f",
-					p1.getName(), val1, val2));
-	}
-
-	private void compare(Integer first, Integer second) {
-		Configuration cfg1 = readcoConfiguration(first);
-		Configuration cfg2 = readcoConfiguration(second);
-		for (Entry<String, Parameter> en : cfg1.getParametersMap().entrySet()) {
-			Parameter p1 = en.getValue();
-			Parameter p2 = cfg2.getParameter(en.getKey());
-			if (p2 == null)
-				throw new IllegalStateException(String.format(
-						"No parameter %s in configuration2", en.getKey()));
-			if (p1.getClass() == Configuration.IntParameter.class)
-				compare((IntParameter) p1, (IntParameter) p2);
-			else if (p1.getClass() == Configuration.FloatParameter.class)
-				compare((FloatParameter) p1, (FloatParameter) p2);
-			else if (p1.getClass() == Configuration.SwitchParameter.class)
-				compare((SwitchParameter<?>) p1, (SwitchParameter<?>) p2);
-			else
-				System.out.println(String.format(
-						"Parameter class %s is not handled.", p1.getClass()
-								.getName()));
-
-		}
-	}
-
-	/*
-	 * Any way to avoid code duplication in compare(IntParameter p1,
-	 * IntParameter p2) and compare(FloatParameter p1, FloatParameter p2)?
-	 */
-	/**
-	 * 
-	 * @param p1
-	 * @param p2
-	 */
-	private static void compare(IntParameter p1, IntParameter p2) {
-		int val1 = p1.getValue();
-		int val2 = p2.getValue();
-		if (val1 == val2)
-			System.out.println(String.format("%s: p1 = p2. value = %d",
-					p1.getName(), val1));
-		if (val1 > val2)
-			System.out.println(String.format("%s: p1 > p2. %d > %d",
-					p1.getName(), val1, val2));
-		else
-			System.out.println(String.format("%s: p1 < p2. %d < %d",
-					p1.getName(), val1, val2));
-	}
-
-	private static <T1, T2> void compare(SwitchParameter<T1> p1,
-			SwitchParameter<T2> p2) {
-		Class<T1> type1 = p1.getGenericParameter();
-		Class<T2> type2 = p2.getGenericParameter();
-		assert type1 == type2;
-		T1 val1 = p1.getValue();
-		T2 val2 = p2.getValue();
-
-		if (val1.equals(val2))
-			System.out.println(String.format(
-					"%s - same values - p1 = %s, p2 = %s. Universe:%s",
-					p1.getName(), val1, val2, p1.getUniverse()));
-		else
-			System.out.println(String.format(
-					"%s - different values - p1 = %s, p2 = %s. Universe:%s",
-					p1.getName(), val1, val2, p1.getUniverse()));
 	}
 
 	private static SqliteAdapter connectDB(String appName) {
