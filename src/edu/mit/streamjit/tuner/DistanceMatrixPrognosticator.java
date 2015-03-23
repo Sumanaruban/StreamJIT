df1329a00d9234de33e8563a4d0e33a2c9e92236
sumanan <sumanan@csail.mit.edu>
2015-03-23 15:53:48 +0800

    DistanceMatrixPrognosticator initializes.
    Initilizes fullParameterSummary.

--- src/edu/mit/streamjit/tuner/DistanceMatrixPrognosticator.java
+++ src/edu/mit/streamjit/tuner/DistanceMatrixPrognosticator.java
@@ -1,6 +1,7 @@
 package edu.mit.streamjit.tuner;
 
 import edu.mit.streamjit.impl.common.Configuration;
+import edu.mit.streamjit.tuner.ConfigurationAnalyzer.FullParameterSummary;
 
 /**
  * Calculates a weighted distance between the current configuration and the
@@ -13,12 +14,25 @@ import edu.mit.streamjit.impl.common.Configuration;
 public class DistanceMatrixPrognosticator implements
 		ConfigurationPrognosticator {
 
+	Configuration prevConfig = null;
+	double prevConfigTime = -1;
+	FullParameterSummary fullParameterSummary;
+
 	@Override
 	public boolean prognosticate(Configuration config) {
+		if (fullParameterSummary == null) {
+			fullParameterSummary = new FullParameterSummary(config);
+			prevConfig = config;
+			return true;
+		}
+
+		ComparisionSummary summarry = ComparisionSummary.compare(config,
+				prevConfig, fullParameterSummary);
 		return false;
 	}
 
 	@Override
 	public void time(double time) {
+		prevConfigTime = time;
 	}
 }
