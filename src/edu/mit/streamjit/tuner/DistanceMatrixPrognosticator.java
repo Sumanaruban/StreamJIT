b7b4f53586d2e8f2e516902a009cde9c4c0025d3
sumanan <sumanan@csail.mit.edu>
2015-03-24 09:31:13 +0800

    DistanceMatrixProg writes the summary to file.

--- src/edu/mit/streamjit/tuner/DistanceMatrixPrognosticator.java
+++ src/edu/mit/streamjit/tuner/DistanceMatrixPrognosticator.java
@@ -1,6 +1,9 @@
 package edu.mit.streamjit.tuner;
 
+import java.io.OutputStreamWriter;
+
 import edu.mit.streamjit.impl.common.Configuration;
+import edu.mit.streamjit.impl.distributed.common.Utils;
 import edu.mit.streamjit.tuner.ConfigurationAnalyzer.FullParameterSummary;
 
 /**
@@ -18,6 +21,16 @@ public class DistanceMatrixPrognosticator implements
 	double prevConfigTime = -1;
 	FullParameterSummary fullParameterSummary;
 
+	private final OutputStreamWriter writer;
+
+	public DistanceMatrixPrognosticator(String appName) {
+		this(Utils.fileWriter(appName, "DistanceMatrix.txt"));
+	}
+
+	public DistanceMatrixPrognosticator(OutputStreamWriter osWriter) {
+		this.writer = osWriter;
+	}
+
 	@Override
 	public boolean prognosticate(Configuration config) {
 		if (fullParameterSummary == null) {
@@ -26,7 +39,7 @@ public class DistanceMatrixPrognosticator implements
 			return true;
 		}
 
-		ComparisionSummary summarry = ComparisionSummary.compare(config,
+		ComparisionSummary summary = ComparisionSummary.compare(config,
 				prevConfig, fullParameterSummary);
 		return false;
 	}
