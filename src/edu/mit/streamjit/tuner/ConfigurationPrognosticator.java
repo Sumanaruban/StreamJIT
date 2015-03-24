07a9502d6cf3cba07d75bc314f467a179299eb8f
sumanan <sumanan@csail.mit.edu>
2015-03-24 10:02:09 +0800

    ManyPrognosticators added.

--- src/edu/mit/streamjit/tuner/ConfigurationPrognosticator.java
+++ src/edu/mit/streamjit/tuner/ConfigurationPrognosticator.java
@@ -1,5 +1,7 @@
 package edu.mit.streamjit.tuner;
 
+import com.google.common.collect.ImmutableSet;
+
 import edu.mit.streamjit.impl.common.Configuration;
 
 /**
@@ -49,6 +51,42 @@ public interface ConfigurationPrognosticator {
 
 		@Override
 		public void time(double time) {
+		}
+	}
+
+	/**
+	 * ManyPrognosticators calls set of {@link ConfigurationPrognosticator}s.
+	 */
+	public static final class ManyPrognosticators implements
+			ConfigurationPrognosticator {
+
+		private final ImmutableSet<ConfigurationPrognosticator> configProgs;
+
+		public ManyPrognosticators(ConfigurationPrognosticator cp1,
+				ConfigurationPrognosticator cp2,
+				ConfigurationPrognosticator... cps) {
+			ImmutableSet.Builder<ConfigurationPrognosticator> builder = ImmutableSet
+					.builder();
+			builder.add(cp1);
+			builder.add(cp2);
+			builder.add(cps);
+			configProgs = builder.build();
+		}
+
+		@Override
+		public boolean prognosticate(Configuration config) {
+			boolean ret = true;
+			for (ConfigurationPrognosticator cp : configProgs) {
+				ret = ret & cp.prognosticate(config);
+			}
+			return ret;
+		}
+
+		@Override
+		public void time(double time) {
+			for (ConfigurationPrognosticator cp : configProgs) {
+				cp.time(time);
+			}
 		}
 	}
 }
