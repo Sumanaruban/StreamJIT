537d70a3170b1c6124a78abebe832d9fb9dcd43e
sumanan <sumanan@csail.mit.edu>
2015-10-17 13:47:43 +0800

    controller.sh added.

---
+++ scripts/controller.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --tasks-per-node=1
+#SBATCH -N 1 
+#SBATCH --cpu_bind=verbose,cores
+#SBATCH --exclusive
+cd /data/scratch/sumanan/FMRadio
+mkdir -p FMRadioCore
+cd FMRadioCore
+srun python ../lib/opentuner/streamjit/streamjit2.py 12563 &
+cd ..
+srun -l ../bin/java/jdk1.8.0_31/bin/java -Xmx120G -XX:InitialCodeCacheSize=1G -XX:ReservedCodeCacheSize=2G -jar FMRadio.jar 3
