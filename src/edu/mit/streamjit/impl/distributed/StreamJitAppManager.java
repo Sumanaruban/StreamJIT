8762b001eaf96df8f7fe4b56a8cc4dc7096bd202
sumanan <sumanan@csail.mit.edu>
2015-11-05 23:43:35 +0800

    SJIAppMgr.preCompilation() and dynamicCfg() added.

--- src/edu/mit/streamjit/impl/distributed/StreamJitAppManager.java
+++ src/edu/mit/streamjit/impl/distributed/StreamJitAppManager.java
@@ -243,10 +243,7 @@ public class StreamJitAppManager {
 	 * @param appinst
 	 */
 	private void preCompilation(AppInstanceManager aim) {
-		Configuration.Builder builder = aim.appInst.getDynamicConfiguration();
-		aim.addConInfoMap(builder, connectionsInUse());
-		Configuration cfg = builder.build();
-		String jsonStirng = cfg.toJson();
+		String jsonStirng = dynamicCfg(aim);
 		ImmutableMap<Integer, DrainData> drainDataMap = aim.appInst
 				.getDrainData();
 		logger.compilationStarted();
@@ -257,6 +254,14 @@ public class StreamJitAppManager {
 			controller.send(nodeID, new CTRLRMessageElementHolder(json,
 					aim.appInst.id));
 		}
+	}
+
+	private String dynamicCfg(AppInstanceManager aim) {
+		Configuration.Builder builder = aim.appInst.getDynamicConfiguration();
+		aim.addConInfoMap(builder, connectionsInUse());
+		Configuration cfg = builder.build();
+		String jsonStirng = cfg.toJson();
+		return jsonStirng;
 	}
 
 	private Collection<ConnectionInfo> connectionsInUse() {
