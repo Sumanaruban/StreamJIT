261db1a6b6783488f7fc9517efdd982702d3e16d
sumanan <sumanan@csail.mit.edu>
2015-11-05 23:57:07 +0800

    Overloaded method SJITAppMgr.preCompilation() added.
    This new method takes drain data sizes as argument and compiles
    new graph with drain data sizes only.

--- src/edu/mit/streamjit/impl/distributed/StreamJitAppManager.java
+++ src/edu/mit/streamjit/impl/distributed/StreamJitAppManager.java
@@ -256,6 +256,19 @@ public class StreamJitAppManager {
 		}
 	}
 
+	private void preCompilation(AppInstanceManager aim,
+			ImmutableMap<Token, Integer> drainDataSize) {
+		String jsonStirng = aim.dynamicCfg(connectionsInUse());
+		logger.compilationStarted();
+		app.eLogger.bEvent("compilation");
+		for (int nodeID : controller.getAllNodeIDs()) {
+			ConfigurationString json = new ConfigurationString.ConfigurationString2(
+					jsonStirng, ConfigType.DYNAMIC, drainDataSize);
+			controller.send(nodeID, new CTRLRMessageElementHolder(json,
+					aim.appInst.id));
+		}
+	}
+
 	private Collection<ConnectionInfo> connectionsInUse() {
 		Collection<ConnectionInfo> connectionsInUse = null;
 		if (prevAIM != null && prevAIM.conInfoMap != null)
