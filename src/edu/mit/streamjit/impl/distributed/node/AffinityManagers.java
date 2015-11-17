828c04767a7bfda0c062c6384af3ef75feb8d2c5
sumanan <sumanan@csail.mit.edu>
2015-11-17 21:41:25 +0800

    New AffinityManagers added.
    EqualAffinityManager and OneCoreAffinityManager have been added.

--- src/edu/mit/streamjit/impl/distributed/node/AffinityManagers.java
+++ src/edu/mit/streamjit/impl/distributed/node/AffinityManagers.java
@@ -1,5 +1,11 @@
 package edu.mit.streamjit.impl.distributed.node;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 
 import edu.mit.streamjit.impl.blob.Blob.Token;
@@ -24,6 +30,80 @@ public class AffinityManagers {
 		@Override
 		public ImmutableSet<Integer> getAffinity(Token blobID, int coreCode) {
 			return null;
+		}
+	}
+
+	/**
+	 * Allocates only one core per blob. All threads of that blob must use that
+	 * single core.
+	 * 
+	 * @author sumanan
+	 * @since 17 Nov, 2015
+	 */
+	public static class OneCoreAffinityManager implements AffinityManager {
+
+		int i = 0;
+
+		Map<Token, Integer> blobCoreMap = new HashMap<>();
+		@Override
+		public ImmutableSet<Integer> getAffinity(Token blobID, int coreCode) {
+			if (!blobCoreMap.containsKey(blobID))
+				blobCoreMap.put(blobID, i++);
+			return ImmutableSet.of(blobCoreMap.get(blobID));
+		}
+	}
+
+	/**
+	 * Equally divide the available cores to the blobs. Threads of a blob share
+	 * all cores that are allocated to that blob.
+	 * 
+	 * @author sumanan
+	 * @since 17 Nov, 2015
+	 */
+	public static class EqualAffinityManager implements AffinityManager {
+
+		private final int totalBlobs;
+
+		private final int totalCores = Runtime.getRuntime()
+				.availableProcessors();
+
+		Map<Token, List<Integer>> blobCoreMap = new HashMap<>();
+
+		int currentCoreToAllocate = 0;
+
+		final int coresPerBlob;
+
+		int reminder;
+
+		EqualAffinityManager(int totalBlobs) {
+			if (totalBlobs == 0)
+				totalBlobs = 1;
+			this.totalBlobs = totalBlobs;
+			coresPerBlob = totalCores / totalBlobs;
+			reminder = totalCores % totalBlobs;
+		}
+
+		@Override
+		public ImmutableSet<Integer> getAffinity(Token blobID, int coreCode) {
+			if (!blobCoreMap.containsKey(blobID))
+				blobCoreMap.put(blobID, newAllocation());
+			return ImmutableSet.copyOf(blobCoreMap.get(blobID));
+		}
+
+		private List<Integer> newAllocation() {
+			if (totalBlobs < totalCores) {
+				List<Integer> l = new ArrayList<>();
+				for (int i = 0; i < coresPerBlob; i++)
+					l.add(currentCoreToAllocate++);
+				if (reminder > 0) {
+					l.add(currentCoreToAllocate++);
+					reminder--;
+				}
+				return l;
+			} else {
+				int c = currentCoreToAllocate++ % totalCores;
+				return ImmutableList.of(c);
+			}
 		}
 	}
 }
